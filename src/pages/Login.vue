21a574056bb9d7eb04b64d5bbee4434615b80b13
Kantapat <kiieky_kiie@hotmail.com>
2021-04-02 18:53:26 +0700

    complete screen all

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,50 +1,89 @@
 <template>
   <div class="card-body" :style="nav2Styles">
     <div class="container-fluid">
-      <br /><br />
-      <div :style="imageCard" class="container">
+      <br />
+      <div class="col-sm-12 text-center">
+        <h1 class="card-title kanitFonts3" :style="textColerHeader">
+          ยินดีต้อนรับ
+        </h1>
+      </div>
+      <div class="container" :style="imageCard">
         <div class="wrapper-full-page" :style="marginToTop">
           <div class="form-group">
             <div class="col-sm-6">
-              <label for="exampleInputEmail1" :style="labelStyleInput"
-                >เบอร์โทรศัพท์</label
+              <label class="kanitFonts2" :style="labelStyleInput"
+                >ชื่อเข้าระบบ</label
               >
             </div>
             <input
-              type="email"
-              class="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
+              type="search"
+              class="form-control kanitFonts1"
               placeholder="User Id"
               :style="inputStyle"
             />
           </div>
           <div class="form-group">
             <div class="col-sm-6">
-              <label for="exampleInputEmail1" :style="labelStyleInput"
+              <label class="kanitFonts2" :style="labelStyleInput"
                 >รหัสผ่าน</label
               >
             </div>
             <input
-              type="email"
-              class="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
+              type="search"
+              class="form-control kanitFonts1"
               placeholder="Password"
               :style="inputStyle"
             />
           </div>
           <div :style="itemTextCenter">
-            <a class="navbar-brand" href="#/admin/confirm-register">
+            <a class="navbar-brand" href="#/admin/account">
               <img
                 class="avatar border-gray"
-                src="img/newImage/BT_GotoMenu.png"
+                src="img/newImage/BT_Login.png"
                 alt="..."
                 :style="imageGotoMainmenu"
               />
             </a>
           </div>
         </div>
+      </div>
+      <div class="row justify-content-md-center">
+        <div class="col-sm-12 text-center">
+          <a class="navbar-brand" href="#/admin/forgot-password">
+            <label class="card-title kanitFonts1" :style="textFooterBoxLogin">
+              ลืมรหัสผ่าน
+            </label>
+          </a>
+          <a class="navbar-brand" href="#">
+            <label class="card-title kanitFonts1" :style="textFooterBoxLogin">
+              / ติดต่อเจ้าหน้าที่
+            </label>
+          </a>
+        </div>
+      </div>
+      <br /><br />
+      <div class="row justify-content-md-center">
+        <div class="col-sm-5">
+          <hr class="new5" />
+        </div>
+        <div class="col-sm-2 text-center">
+          <label class="card-title kanitFonts1" :style="textFooterBoxLogin">
+            ยังไม่มีบัญชี
+          </label>
+        </div>
+        <div class="col-sm-5">
+          <hr class="new5" />
+        </div>
+      </div>
+      <br /><br />
+      <div class="text-center">
+        <a class="navbar-brand" href="#/admin/register">
+          <img
+            class="img-responsive"
+            src="img/newImage/BT_Register.png"
+            :style="imgRegister"
+          />
+        </a>
       </div>
     </div>
   </div>
@@ -60,30 +99,15 @@ export default {
         backgroundColor: "black",
         height: "100%"
       },
-      imageUser: {
-        width: "100px"
-      },
-      imageCardCenter: {
-        width: "90%"
-      },
-      inputViewUser: {
-        width: "90%",
-        height: "45%",
-        marginTop: "25px",
-        backgroundColor: "#383838",
-        borderRadius: "4px",
-        color: "white"
-      },
-      textCenter1: {
-        textAlign: "center",
+      textColerHeader: {
         color: "#ffd373"
+      },
+      textFooterBoxLogin: {
+        color: "#757575",
+        fontSize: "24px"
       },
       itemTextCenter: {
         textAlign: "center"
-      },
-      textCenter2: {
-        textAlign: "center",
-        color: "white"
       },
       marginToTop: {
         paddingTop: "25px"
@@ -97,34 +121,32 @@ export default {
         backgroundColor: "black",
         borderWidth: "0px",
         margin: "25px",
-        width: "90%",
-        color: "white"
+        width: "95%",
+        color: "white",
+        fontSize: "22px"
       },
       imageGotoMainmenu: {
-        width: "25%"
+        width: "250px"
       },
       imageCard: {
-        width: "100%",
-        height: "100%",
-        backgroundRepeat: "no-repeat",
-        backgroundSize: "cover",
-        backgroundImage: "url(img/newImage/BG_Detail01.png)"
+        backgroundImage: "url(img/newImage/BG_Detail01.png)",
+        marginTop: "5%",
+        marginBottom: "5%",
+        backgroundSize: "contain no-repeat",
+        // backgroundPosition: "center",
+        width: "1016px",
+        height: "407px"
       },
-      imageLogoUser: {
-        backgroundImage: "url(img/newImage/Profile_Picture.png)"
+      imgRegister: {
+        width: "350px"
       }
     };
   }
 };
 </script>
-<style lang="scss" scoped>
-// .imageLogoUserClass {
-//   height: 120px;
-//   width: 120px;
-//   background-repeat: no-repeat;
-//   background-size: cover;
-//   @media (max-width: 988px) {
-//     background-size: cover;
-//   }
-// }
+<style lang="scss">
+hr.new5 {
+  border: 1px solid white;
+  border-radius: 0px;
+}
 </style>
